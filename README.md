d9a00fe57d58ecfbcd5c5934dfbd605b01cf54ef
aokihu <aokihu@gmail.com>
2015-09-05 18:31:58 +0800

    init
    项目初始化

---
+++ README.md
@@ -0,0 +1,4 @@
+Baidu Yuyin 百度语音合成
+-----------------------
+
+使用Baidu的在线语音合成服务，需要自己申请API Key，申请网址http://yuyin.baidu.com
---
+++ index.js
@@ -0,0 +1,8 @@
+/**
+ * Baidu 语音合成
+ *
+ * @author aokihu(aokihu@gmail.com)
+ * @version 1.0.0
+ * @description 需要自己提供API的key,网址http://yuyin.baidu.com
+ *
+ */
