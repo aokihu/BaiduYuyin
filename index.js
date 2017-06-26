bb9a628af95806fd15f25ff2c38cccd3997017d8
aokihu <aokihu@gmail.com>
2017-06-27 01:03:45 +0800

    缺少连续语音播放的方法

--- index.js
+++ index.js
@@ -6,112 +6,184 @@
  * @description 需要自己提供API的key,网址http://yuyin.baidu.com
  *
  */
-'use strict'
+"use strict";
 
-const eventEmitter = require('events').EventEmitter
-const util   = require('util')
-const querystring = require('querystring')
-const request = require('request')
-const md5 = require('md5')
-const fs = require('fs')
-const child_process = require('child_process')
+const eventEmitter = require("events").EventEmitter;
+const util = require("util");
+const request = require("request");
+const querystring = require("querystring");
+const md5 = require("md5");
+const fs = require("fs");
+const child_process = require("child_process");
 
+// 获取Token的url
+const AccessUrl = "https://openapi.baidu.com/oauth/2.0/token";
+// 百度语音合成url
+const BDSpeechUrl = "http://tsn.baidu.com/text2audio";
+// Session信息
+const SessionFile = __dirname + "/session.json";
+// Session超时设定为12小时
+const TokenDeadline = 12 * 3600 * 1000;
+
+const BDID = '123ff993dd';
 
 /**
  * 百度语音
  */
 
-class BDSpeech extends eventEmitter{
-
+class BDSpeech extends eventEmitter {
   /**
    * 构造方法
    * @method constructor
-   * @param  {string}    apiKey        自己去看百度文档
-   * @param  {string}    secrectKey    自己去看百度文档
-   * @param  { string}   playCmd       播放器的命令
-   * @param  {string}    path          保存数据的路径
-   * @param  {boolean}   bufferd=false 是否要保存到本地缓存起来
-   * @return {[type]}                  [description]
+   * @param  {String}    apiKey        自己去看百度文档
+   * @param  {String}    secrectKey    自己去看百度文档
+   * @param  {String}    playCmd       播放器的命令
+   * @param  {String}    path          保存数据的路径
+   * @param  {Boolean}   bufferd=false 是否要保存到本地缓存起来
+   * @return {BDSpeech}                  [description]
    */
 
-  constructor (apiKey, secrectKey, playCmd="afplay", path, bufferd=false) {
+  constructor(apiKey, secrectKey, playCmd = "afplay", path, bufferd = false) {
+    super();
 
-    super()
+    const client_id = apiKey;
+    const client_secret = secrectKey;
+    const grant_type = "client_credentials";
 
-    let __accessUrl__ = 'https://openapi.baidu.com/oauth/2.0/token';
-    this.__url__ = 'http://tsn.baidu.com/text2audio'
+    this.bufferd = bufferd; // 是否缓存音乐
+    this.bufferdPath = __dirname + "/download";
+    this.sessionToken = "";
+    this.isLogin = false;
+    this.sessionFile = __dirname + "/session.json";
+    this.tempFile = (path ? path : __dirname) + "/temp.mp3";
+    this.playCmd = playCmd;
 
-    let client_id = apiKey
-    let client_secret = secrectKey
-    let grant_type = 'client_credentials'
-
-    this.bufferd = bufferd // 是否缓存音乐
-    this.bufferdPath = __dirname + '/download'
-    this.sessionToken = ''
-    this.isLogin = false
-    this.sessionFile = __dirname + '/session.json'
-    this.tempFile = (path ? path :  __dirname) + '/temp.mp3'
-    this.playCmd = playCmd
-
-    let params = {
-      'grant_type':grant_type,
-      'client_id':client_id,
-      'client_secret':client_secret
-    }
-
-    fs.access(this.sessionFile, fs.F_OK, err => {
-
-      if(err){
-
-        // 从百度获取token session
-        var _url = __accessUrl__ + "?" + querystring.stringify(params);
-
-        request(_url, (err, res, body) => {
-          let json = JSON.parse(body)
-          let _token = json.access_token
-
-          this.sessionToken = _token
-          this.isLogin = true
-
-          // write session token to local file
-          fs.writeFile(this.sessionFile, JSON.stringify({token:_token}), err => {
-            if(err) throw err
-          })
-
-          this.emit('ready', _token)
-        });
-
-      }
-      else{
-
-        // read bufferd token session
-        let _sessionJson = JSON.parse(fs.readFileSync(this.sessionFile));
-        let sessionToken = _sessionJson.token;
-
-        this.sessionToken = sessionToken
-        this.isLogin = true
-
-        this.emit('ready', sessionToken)
-      }
-
-    })
-
+    // 获取Token
+    this.vaildToken()
+      .then(this.tokenReady.bind(this))
+      .catch(() => {
+        const params = { grant_type, client_id, client_secret };
+        return this.requestToken(params);
+      })
+      .then(this.tokenReady.bind(this))
+      .catch(console.error);
 
     // 处理缓存
-    if(this.bufferd){
-
+    if (this.bufferd) {
       // 检查缓存目录是否存在
       // 不存在就创建一个
       fs.access(this.bufferdPath, fs.F_OK, err => {
-
-        if(err)
-        {
-          fs.mkdir(this.bufferdPath)
+        if (err) {
+          fs.mkdir(this.bufferdPath);
         }
+      });
+    }
+  }
 
+  /**
+   * @method 验证Token的有效性
+   * 因为token会过期，之前没有注意到
+   * 因此当过期的token存的时候，无法正常获取语音文件
+   */
+  vaildToken() {
+    return new Promise((resolve, reject) => {
+      fs.access(SessionFile, fs.R_OK, err => {
+        if (err) {
+          // TODO: 如果没有Session文件，那么返回false
+          reject();
+        } else {
+          // TODO: 如果有Session文件，然后检查是否已经过期
+          const { token, timestamp } = JSON.parse(fs.readFileSync(SessionFile));
+          const delta = new Date() - new Date(timestamp);
+          delta > TokenDeadline ? reject() : resolve(token);
+        }
+      });
+    });
+  }
+
+  /**
+   * @method 获取新的token
+   */
+  requestToken(params) {
+    // 从百度获取token session
+    const _url = AccessUrl + "?" + querystring.stringify(params);
+
+    return new Promise((resolve, reject) => {
+      request(_url, (err, res, body) => {
+        const { access_token: token } = JSON.parse(body);
+        this.saveToken(token).then(resolve).catch(reject);
+      });
+    });
+  }
+
+  /**
+   * Token准备完毕
+   * @param {String} token Session Token
+   */
+  tokenReady(token) {
+    this.sessionToken = token;
+    this.isLogin = true;
+    this.emit("ready", token);
+  }
+
+  /**
+   * 保存Token
+   * @param {String} token Session Token
+   */
+  saveToken(token) {
+    return new Promise((resolve, reject) => {
+      fs.writeFile(
+        SessionFile,
+        JSON.stringify({ token, timestamp: new Date() }),
+        err => {
+          if (err) {
+            reject(err);
+          } else {
+            resolve(token);
+          }
+        }
+      );
+    });
+  }
+
+  /**
+   * 大声朗读
+   * @param {String} file 语音文件
+   */
+  loudSpeak(file) {
+    return new Promise((resolve, reject) => {
+      try {
+        fs.accessSync(file, fs.F_OK | fs.R_OK);
+        child_process.spawn(this.playCmd, [file]).on("exit", (code, signal) => {
+          resolve();
+        });
+      } catch (err) {
+        reject(err);
+      }
+    });
+  }
+
+  /**
+   * 下载语音文件
+   */
+  downloadSpeechFile(file, url){
+
+    return new Promise((resolve, reject) => {
+
+      const download = fs.createWriteStream(file);
+
+      download.on("finish", () => {
+        resolve(file);
+      });
+
+      download.on('error', err => {
+        reject(err);
       })
 
-    }
+      // download text audio file
+      request(url).pipe(download);
+
+    })
 
   }
 
@@ -123,70 +195,39 @@ class BDSpeech extends eventEmitter{
    * @return {[type]}     [description]
    */
 
-  speak(txt, opt){
-
-    var id = 'fsu77883jjfkfkkf' //(new Date()).getTime();
+  speak(txt, opt) {
+    var id = BDID;
     var params = {
-      'tex':encodeURIComponent(txt),
-      'lan':'zh',
-      'tok':this.sessionToken,
-      'ctp':1,
-      'cuid':'BDS-'+id,
-      'spd':4,
-      'pit':4,
-      'vol':5,
-      'per':0
+      tex: encodeURIComponent(txt),
+      lan: "zh",
+      tok: this.sessionToken,
+      ctp: 1,
+      cuid: "BDS-" + id,
+      spd: 4,
+      pit: 4,
+      vol: 5,
+      per: 0
+    };
+
+    let url = BDSpeechUrl + "?" + querystring.stringify(params);
+    let dl = md5(url);
+    // 语音文件
+    const bufferFile = this.bufferdPath + "/" + dl;
+    const speechFile = this.bufferd ? bufferFile : this.tempFile;
+
+    if(this.bufferd){
+      return this.loudSpeak(speechFile)
+      .catch(err => {
+        this.downloadSpeechFile(speechFile, url)
+        .then(file => {return this.loudSpeak(file)})
+        .catch(console.log)
+      });
+    } else {
+      return this.downloadSpeechFile(speechFile, url)
+      .then(file => {return this.loudSpeak(file)})
+      .catch(console.log)
     }
-
-    let url = this.__url__ + "?" + querystring.stringify(params)
-    let dl = md5(url)
-
-    return new Promise((resolve, reject) => {
-
-      if(this.bufferd){
-        try{
-          fs.accessSync(this.bufferdPath + '/' + dl,  fs.F_OK|fs.R_OK)
-          child_process.spawn(this.playCmd, [this.bufferdPath + '/' + dl])
-          .on('exit', (code, signal) => resolve())
-          return true
-
-        }catch(err){
-          reject(err);
-        }
-      }
-
-      // download file pipeline
-      let dlFile = this.bufferd ? this.bufferdPath + '/' + dl : this.tempFile
-      let download = fs.createWriteStream(dlFile)
-
-      download.on('finish', ()=>{
-
-        child_process.spawn(this.playCmd,[dlFile])
-        .on('exit', (code, signal) => {
-          // remove temp muisc file
-          if(!this.bufferd){
-          // check file is exist
-            try{
-              fs.accessSync(this.bufferdPath + '/' + dl,  fs.F_OK|fs.R_OK)
-              fs.unlinkSync(dlFile)
-            }
-            catch(err){
-              console.log(err)
-            }
-            
-          }
-          resolve()
-        })
-
-      })
-
-      // download text audio file
-      request(url).pipe(download)
-
-    });
-
   }
-
 }
 
-module.exports = BDSpeech
+module.exports = BDSpeech;
--- test.js
+++ test.js
@@ -1,8 +1,8 @@
 var BDSpeech = require("./index.js")
 
-var speech = new BDSpeech("DAZTeCYAkuAQwtPvWDcBb9Ih", "adb28deb5103bde96bd9633dd437ff2d", 'afplay', './download', true);
+var speech = new BDSpeech("DAZTeCYAkuAQwtPvWDcBb9Ih", "adb28deb5103bde96bd9633dd437ff2d", 'afplay', './download');
 speech.on('ready', token => {
-
+  console.log(token);
   // var now = new Date();
   // var time = now.getHours()+"点"+now.getMinutes()+"分"
   // let hour = now.getHours()
@@ -24,9 +24,10 @@ speech.on('ready', token => {
   // speech.speak(`${daytime}好,现在时间是${daytime}${time}`)
   //
 
-  // speech.speak('你好，欢迎光临利生宝公共广播系统')
-  // .then(() => {return speech.speak('我公司集合研发、生产、销售于一体')})
-  // .then(() => speech.speak('生产销售适用于公用场所、家居生活的音响设施'))
+  speech.speak('你好，欢迎光临利生宝公共广播系统')
+  .then(() => {return speech.speak('我公司集合研发、生产、销售于一体')})
+  .then(() => speech.speak('生产销售适用于公用场所、家居生活的音响设施'))
+  .catch(console.error)
 
-  speech.speak('切换到D L N A模式')
+  // speech.speak('切换到D L N A模式')
 })
